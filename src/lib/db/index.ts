dbb9418af00b73b869bb10e8526fd99abd813ca8
wanto-production <ikwansatria3974@gmail.com>
2026-04-25 16:54:57 +0700

    update

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,7 +2,6 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import { buildSchema } from "drizzle-graphql";
 import { createYoga } from "graphql-yoga";
-import { auth } from "../auth/auth.ts";
 import * as DBschema from "./schema.ts";
 
 const client = createClient({
@@ -15,15 +14,6 @@ export const db = drizzle(client, { schema: DBschema });
 const { schema } = buildSchema(db);
 export const yoga = createYoga({
   schema,
-  context: async ({ request }) => {
-    const session = await auth.api.getSession({
-      headers: request.headers,
-    });
-    return {
-      user: session?.user ?? null,
-      session: session?.session ?? null,
-    };
-  },
   graphqlEndpoint: "/api/graphql",
   landingPage: true,
 });
